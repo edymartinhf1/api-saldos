0bf1ed1d0fe2269f1896db1302bdecc4ac35fd73
edymartinhf1 <edymartinhf1@gmail.com>
2023-07-17 17:19:06 -0500

    sonar fix

--- src/test/java/com/bootcamp/bank/ApiSaldosApplicationTests.java
+++ src/test/java/com/bootcamp/bank/ApiSaldosApplicationTests.java
@@ -1,13 +1,23 @@
 package com.bootcamp.bank;
 
+import com.bootcamp.bank.saldos.service.SaldoResumenService;
+import com.bootcamp.bank.saldos.service.SaldoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class ApiSaldosApplicationTests {
-
+	@Autowired
+	private SaldoResumenService saldoResumenService;
+	@Autowired
+	private SaldoService saldoService;
 	@Test
 	void contextLoads() {
+		assertThat(saldoResumenService).isNotNull();
+		assertThat(saldoService).isNotNull();
 	}
 
 }
